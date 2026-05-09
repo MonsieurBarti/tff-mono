e94542866a45d0847e40bb8e75dba8609ea4d484
MonsieurBarti <github@barti.mozmail.com>
2026-05-09 21:11:35 +0200

    feat(S02/T17): export domain layer from shared-core
    Create barrel index.ts files in each domain subdirectory exporting
    public APIs, and update root src/index.ts to re-export from all
    domain modules. Alias colliding InvalidTransitionError exports from
    milestone and slice domains.

---
+++ packages/shared-core/src/domain/project/index.ts
@@ -0,0 +1,6 @@
+export { Project, type ProjectState } from "./project.entity.js";
+export { ProjectCreatedEvent } from "./project-created.event.js";
+export { ProjectVisionUpdatedEvent } from "./project-vision-updated.event.js";
+export { ProjectNameUpdatedEvent } from "./project-name-updated.event.js";
+export { ProjectExistsError } from "./project.error.js";
+export { ProjectRepository } from "./project.repository.js";
